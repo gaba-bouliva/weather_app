41cffcb2f46deee21c9fa435d40596d4d5808d6b
gaba-bouliva <eaglebald111@gmail.com>
2023-06-06 05:51:22 +0100

    Display weather forecast for next 5 days

--- src/app.js
+++ src/app.js
@@ -1,12 +1,3 @@
-/**
- * TODO
- * 1 get user current location
- * 2 get user current location weather fromm weather api
- * 3 populate various weather info on weather app
- * 4 populate corresponding weather icons base on weather
- * 4 implement search location city/country
- */
-
 const envVariables = process.env;
 let currentLocation = null;
 let weatherInfo = null;
@@ -17,10 +8,8 @@ const {
   APP_ENV,
   APP_WEATHER_API_KEY,
   APP_WEATHER_API_ENDPOINT,
-  APP_GIPHY_API_KEY,
-  APP_GIPHY_API_ENDPOINT,
   APP_IP_INFO_API_KEY,
-  APP_IP_INFO_ENDPOINT
+  APP_IP_INFO_ENDPOINT,
 }  = envVariables;
 
 async function fetchUserCurrentLocation() {
@@ -33,6 +22,7 @@ async function fetchUserCurrentLocation() {
       }
     });
     const data = await response.json();
+    
     console.log('user location data: ', data);
     if (data.hasOwnProperty('error')) {
       throw(error)
@@ -46,12 +36,12 @@ async function fetchUserCurrentLocation() {
 
 }
 
-async function fetchWeatherData(location) {
-  // retrieves provided location weather info
-  console.log('weatherLocation: ', location);
+
+async function fetchWeatherForecast(location, nbrDays) {
+  // returns weather forecast for the  location and number of days provided
   try {
-  
-    const response = await fetch(`${APP_WEATHER_API_ENDPOINT}?q=${location.city}&key=${APP_WEATHER_API_KEY}`, {
+
+    const response = await fetch(`${APP_WEATHER_API_ENDPOINT}?q=${location.city}&key=${APP_WEATHER_API_KEY}&days=${nbrDays}&aqi=no&alerts=no`, {
       mode: 'cors',
       headers: {
           'Content-Type': 'application/json',
@@ -62,8 +52,10 @@ async function fetchWeatherData(location) {
     if (data.hasOwnProperty('error')) {
       throw(error)
     }else{
-      setWeatherInfo(JSON.stringify(data));
+      console.log('Weather forecast info: ', data);
+      setWeatherForecastInfo(JSON.stringify(data));
     }
+    
   } catch (error) {
     throw(error);
   }
@@ -72,42 +64,93 @@ async function fetchWeatherData(location) {
 function getCurrentLocation () {
   // return user location data cached in browser local storage
   let locationData = JSON.parse(localStorage.getItem('location'));
-  if ( locationData.hasOwnProperty('city') && locationData.hasOwnProperty('country')) {
+  if ( locationData && locationData.hasOwnProperty('city') && locationData.hasOwnProperty('country')) {
     
     return {'city': locationData.city.name, 'country': locationData.country.name};
     //return {'city': 'Milan', 'country': 'Italy'};
     
   } else { 
-    // fetches the user location data if not available in local storage 
+    
     fetchUserCurrentLocation()
   }
 }
 
-function getLocationWeather (location) {
-  // return weather data cached in browser local storage
-  let weatherData = JSON.parse(localStorage.getItem('weatherInfo'));
-  if ( weatherData && 
-    weatherData.location.name === currentLocation.city &&
-    weatherData.location.country === currentLocation.country
-         ) {
-    return weatherData
+function getLocationWeatherForecast(location, nbrDays) {
+  let weatherForecast = JSON.parse(localStorage.getItem('weatherInfo'));
+  if ( weatherForecast ) {
+    return weatherForecast;
   } else {
-    fetchWeatherData(location)
+    fetchWeatherForecast(location, nbrDays);
   }
 }
 
 function setLocation (location) {
   // saves/caches current user location data to browser local storage
- localStorage.setItem('location', location)
+ localStorage.setItem('location', location);
 }
 
-function setWeatherInfo (weatherData) {
-  // save/cache current weather info to local storage
-  localStorage.setItem('weatherInfo', weatherData )
+function setWeatherForecastInfo (weatherForecastData) {
+  
+  localStorage.setItem('weatherInfo', weatherForecastData);
+}
+
+function displayWeatherForecast (weatherForecasts) {
+  /**
+   * displays next x days weather forecast
+   */
+  for (let i = 0; i < weatherForecasts.length; i++) {
+
+    if (i === 0) {
+      // ignore first forecast (current day forecast)
+      continue;
+    }
+    const forecast = weatherForecasts[i];
+
+    let forecastDiv = document.querySelector(`.today-plus-${i}`);
+
+    forecastDiv.innerHTML = `
+      <h3>${forecast.date}</h3>
+      <div class="weather-icon">
+        <img src="${forecast.day.condition.icon}" class="${forecast.day.condition.text}">
+      </div>
+      <p class="temp">
+            <span class="min-temp">
+              ${ temperatureUnit === 'f' ? forecast.day.mintemp_f : forecast.day.mintemp_c } 
+              ${ temperatureUnit === 'f' ? "<span class='temp-unit'><sup>&deg;</sup>F</span>" : "<span class='temp-unit'><sup>&deg;</sup>C</span> " }
+            </span>
+            <span class="max-temp">
+            ${ temperatureUnit === 'f' ? forecast.day.maxtemp_f : forecast.day.maxtemp_c } 
+            ${ temperatureUnit === 'f' ? "<span class='temp-unit'><sup>&deg;</sup>F</span>" : "<span class='temp-unit'><sup>&deg;</sup>C</span> " } 
+            </span>
+      </p>
+    `
+    
+  }
 }
 
 
-function displayWeather (weatherData) {
+function displayCurrentWeatherHighlights(currentWeather) {
+  let windStatusP = document.querySelector('.wind-status');
+  let humidityP = document.querySelector('.humidity-status');
+  let humidityBarDiv = document.querySelector('.humidity-bar') 
+  let visibilityP = document.querySelector('.visibility-status');
+  let airPressureP = document.querySelector('.air-pressure-status');
+
+  windStatusP.innerHTML = `${currentWeather.wind_mph}<span>mph</span>`;
+  humidityP.innerHTML = `${currentWeather.humidity}<span>%</span>`;
+
+  humidityBarDiv.style.backgroundImage = `linear-gradient(to right, yellow 
+                                          ${currentWeather.humidity}%, rgba(0,0,0,0) 
+                                          ${currentWeather.humidity}%)`
+
+  visibilityP.innerHTML =  `${currentWeather.vis_miles}<span>miles</span>`;
+  airPressureP.innerHTML = `${currentWeather.pressure_mb}<span>mb</span>`;
+  
+
+  
+}
+
+function displayCurrentLocationWeather (currentWeather, location) {
   let currentWeatherImg = document.querySelector('.current-weather-icon');
   let currentTemperatureH1 = document.querySelector('.current-temp');
   let weatherDescriptionH2 = document.querySelector('.weather-description');
@@ -119,45 +162,40 @@ function displayWeather (weatherData) {
   let month = currentDateArr[1];
   let dayOfWeek = currentDateArr[0];
 
-  //currentWeatherImg.setAttribute('src', `${weatherData.current.condition.icon}`);
-  let weatherImgSrc = {'rain': 'HeavyRain.png', 'cloud': 'HeavyCloud.png', 'sun': 'Clear.png', 'sunny': 'Clear.png', 'rainy': 'HeavyRain.png', 'snow': 'Snow.png'}
-
-  let weatherDescArr = weatherData.current.condition.text.toLocaleLowerCase().split(' ');
-
-  for (let i = 0; i < weatherDescArr.length; i++) {
-    const word = weatherDescArr[i];
-    if (weatherImgSrc[word]) {
-      currentWeatherImg.setAttribute('src', `images/${weatherImgSrc[word]}`);
-      break;
-    }
-  }
+  currentWeatherImg.setAttribute('src', `${currentWeather.condition.icon}`);
+  currentWeatherImg.setAttribute('alt', `${currentWeather.condition.text}`);
+  currentWeatherImg.style.width = '100px';
+  currentWeatherImg.style.zIndex = '1';
 
   if ( temperatureUnit === 'f') {
 
-    currentTemperatureH1.innerHTML  = `${ parseInt(weatherData.current.temp_f)}
+    currentTemperatureH1.innerHTML  = `${ parseInt(currentWeather.temp_f)}
                                       <span class="temp-unit">&deg;F</span>`
   } else {
-    currentTemperatureH1.innerHTML  = `${ parseInt(weatherData.current.temp_c)}
+    currentTemperatureH1.innerHTML  = `${ parseInt(currentWeather.temp_c)}
                                       <span class="temp-unit">&deg;C</span>`
   }
 
-  weatherDescriptionH2.innerText = `${weatherData.current.condition.text}`
+  weatherDescriptionH2.innerText = `${currentWeather.condition.text}`
   todayDateP.innerText = `Today. ${dayOfWeek}, ${dayOfMonth} ${month}`;
-  citySpan.innerText = `${weatherData.location.name}`
+  citySpan.innerText = `${location.city}`
 }
 
 currentLocation = getCurrentLocation()
 
-weatherInfo = getLocationWeather(currentLocation)
+if (currentLocation) {
+  console.log("Current Location Info: ", currentLocation);
+  weatherInfo = getLocationWeatherForecast(currentLocation, 6);
 
-console.log("Current Location Info: ", currentLocation);
-console.log('Weather Info: ', weatherInfo);
-
-if (weatherInfo) {
-  
-  displayWeather(weatherInfo)
 } else {
-  throw('Weather Info undefined!')
+  throw('Unknown current location!')
 }
 
-
+if (weatherInfo) {
+  console.log('Weather Forecast Info: ', weatherInfo);
+  displayCurrentLocationWeather(weatherInfo.current, currentLocation);
+  displayWeatherForecast(weatherInfo.forecast.forecastday);
+  displayCurrentWeatherHighlights(weatherInfo.current);
+} else {
+  throw('Weather forecast info undefined!');
+}
